b29e837b997eff8921d21165d09926485f461194
Soliha <solyatursuntasheva@gmail.com>
2022-12-29 16:10:52 +0500

    so tired

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Rout from "./router/router";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/invest">
+    <BrowserRouter basename="/">
       <Rout />
     </BrowserRouter>
   </React.StrictMode>
