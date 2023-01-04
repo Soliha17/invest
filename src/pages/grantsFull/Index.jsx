8ea787cefd13d9a06d6c4e5377a3475d9037048b
Soliha <solyatursuntasheva@gmail.com>
2023-01-04 22:16:16 +0500

    Made Grants Full Page

--- src/pages/grantsFull/Index.jsx
+++ src/pages/grantsFull/Index.jsx
@@ -1,7 +1,8 @@
-import React, { useState,useEffect } from "react";
-import { Link,useNavigate,useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 import "./style.scss";
+
 import { grantsInfo } from "../../utils/grantsInfo";
 import GrantsFeed from "../../components/GrantsFeed/Index";
 
@@ -14,9 +15,8 @@ import sendIcon from "../../assets/icons/send-icon.svg";
 import savedIcon from "../../assets/icons/saved-icon.svg";
 import Sidebar from "../../components/Sidebar/Index";
 import backIcon from "../../assets/icons/back-icon.svg";
-import smallDateIcon from "../../assets/icons/small-date-icon.svg"
+import smallDateIcon from "../../assets/icons/small-date-icon.svg";
 import smallLocationIcon from "../../assets/icons/small-location-icon.svg";
-
 
 function GrantsMain() {
   const [dots, setDots] = useState(false);
@@ -49,7 +49,7 @@ function GrantsMain() {
           </span>
           <div className="grants-container">
             {grants.map((info) => (
-              <span className="grants-box" key={info.id}>
+              <span className="grants-box grants-box__auto" key={info.id}>
                 <span className="grants__top flex">
                   <img src={grantsLogo} alt="grants Logo" />
                   <span className="three-dots">
@@ -102,58 +102,36 @@ function GrantsMain() {
                 <span className="info-box__grants">
                   <span className="first__info-box">
                     <h6 className="f-2431">Batafsil</h6>
-                    <p className="f-2024">
-                      Buyuk Britaniyada joylashgan Oksford universiteti
-                      tarkibidagi Said Business School oliygohi MBA magistrlik
-                      dasturida uzluksiz taʼlim (full-time) asosida tahsil
-                      olishni xohlovchilar uchun har yili Skoll Scholarship
-                      dasturini taklif etadi. Grant butun magistrlik dasturi
-                      davri (1 yil) uchun taqdim etiladi. Har yili 4 nafargacha
-                      talaba ushbu grant bilan taqdirlanadi.
-                    </p>
+                    <p className="f-2024">{info.firstInfo}</p>
                   </span>
                   <span className="second__info-box">
                     <h6 className="f-2431">Ariza topshirish jarayoni</h6>
-                    <p className="f-1621">
-                      Grant arizasi Oksford MBA dasturiga topshiriladigan
-                      hujjatlar bilan birga topshiriladi. Hozirda 2023-yil
-                      sentyabrda boshlanadigan Oksford MBA dasturiga ariza qabul
-                      qilinmoqda va arizalar 4 marta qabul qilinadi. Skoll
-                      Scholarship grantiga ariza topshirmoqchi boʻlganlar
-                      quyidagi uchta qabuldan birida Oksford MBA dasturiga
-                      hujjat topshirish lozim:
-                    </p>
+                    <p className="f-1621">{info.secondInfo}</p>
+                    <ul>
+                      <li>{info.listInfo}</li>
+                      <li>{info.listInfoSecond}</li>
+                      <li>{info.listInfoThird}</li>
+                    </ul>
                   </span>
-                  <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                  </ul>
-                  <h6 className="f-2431">Dasturning manfaatli tomoni</h6>
-                  <p>
-                    Oksford MBA dasturida oʻqish uchun bir yillik kontrakt
-                    toʻlovi 2023-2024-oʻquv yili uchun 75 140 funt-sterlingni
-                    tashkil etadi.Skoll Scholarship grantini qoʻlga kiritgan
-                    talabalar quyidagi imtiyozlar bilan taʼminlanadi:
-                  </p>
+                  <span className="third__info-box">
+                    <h6 className="f-2431">Dasturning manfaatli tomoni</h6>
+                    <p>{info.thirdInfo}</p>
+                    <ul>
+                      <li>{info.firstList}</li>
+                      <li>{info.secondList}</li>
+                      <li>{info.thirdList}</li>
+                    </ul>
+                  </span>
                 </span>
-                <span className="footer__news-main flex">
+                <span className="footer__news-main footer__grants-main flex">
                   <span className="left__footer flex">
-                    <p className="flex f-1417">
-                      <img
-                        src={smallDateIcon}
-                        width="16"
-                        height="16"
-                        alt="eye icon"
-                      />
-                      Tugash vaqti: 04.01.2023
-                    </p>
-                    <p className="flex f-1417">
-                      <img src={smallLocationIcon} alt="location icon" />
-                      Buyuk Britaniya
-                    </p>
+                    <Link className="flex f-2431">Ro‘yxatdan o‘tish</Link>
                   </span>
                   <span className="right__footer flex">
+                    <p className="flex f-1417">
+                      <img src={eyeIcon} alt="eye icon" />
+                      100
+                    </p>
                     <img src={sendIcon} alt="" />
                     <img src={savedIcon} alt="" />
                   </span>
